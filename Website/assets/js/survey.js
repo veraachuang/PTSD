54994c9619d199fd1476b02ad4c9897d269b3721
Spencer Madden <spencm6@uw.edu>
2023-10-14 21:19:47 -0700

    Fixed button on journal page

--- Website/assets/js/survey.js
+++ Website/assets/js/survey.js
@@ -1,4 +1,4 @@
-const moodButtons = document.querySelectorAll('.mood-button');
+const moodButtons = document.querySelectorAll('.mood-button-2');
 
 // Add event listener to each mood button
 moodButtons.forEach(button => {
@@ -9,16 +9,16 @@ moodButtons.forEach(button => {
         // Set response based on selected mood
         switch (mood) {
             case 'happy':
-                response = 'I am glad you feel happy today! Reflect on something that made you feel happy :)';
+                response = 'I am glad you feel happy today! Reflect on something that made you feel happy:)';
                 break;
             case 'sad':
-                response = "I'm sorry to hear that you're feeling sad.Is there anything I can do to help ? ";
+                response = "I'm sorry to hear that you're feeling sad. Is there anything I can do to help? ";
                 break;
             case 'angry':
-                response = "It sounds like you're feeling angry.Take a deep breath and try to find a positive outlet for your emotions.";
+                response = "It sounds like you're feeling angry. Take a deep breath and try to find a positive outlet for your emotions.";
                 break;
             case 'excited':
-                response = "That's great to hear that you 're feeling excited! What's got you so excited ? ";
+                response = "That's great to hear that you're feeling excited! What's got you so excited? ";
                 break;
             default:
                 response = "I want to know more about why you feel that way. Please click the chat box in the bottom right corner to talk!";
