d930f06319df6b5ffd6e871ddc05deacb8fa5b51
vera chuang <verachuanghh0809@gmail.com>
2023-10-14 21:02:03 -0700

    save journal as text files

--- Website/assets/js/journal.js
+++ Website/assets/js/journal.js
@@ -1,8 +1,16 @@
 document.getElementById('save-button').addEventListener('click', function() {
     const journalEntry = document.getElementById('journal-entry').value;
 
-    // Save the journal entry to localStorage or send it to a server
-    // You can customize this part based on your requirements
+    // Create a new Blob object with the journal entry content
+    const blob = new Blob([journalEntry], { type: 'text/plain' });
+
+    // Create a new anchor element to download the file
+    const anchor = document.createElement('a');
+    anchor.href = URL.createObjectURL(blob);
+    anchor.download = 'journal-entry.txt';
+
+    // Simulate a click on the anchor element to trigger the download
+    anchor.click();
 
     // Clear the journal entry textarea
     document.getElementById('journal-entry').value = '';
